8dfee23ac2a221c46855ad42b8cd6ef3443de15e
Mitali Awasthi <mitaliawasthi131@gmail.com>
2026-04-27 17:21:54 +0530

    chore: load environment variables and validate DATABASE_URL in database setup

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,8 +1,16 @@
+from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL missing")
+
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
